07d46f7458e17f81947c53289e9d3ea0b6e291df
krystofprihoda <prihoda46@gmail.com>
2025-04-11 10:38:22 +0200

    [Server] .toml simplified

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -37,34 +37,3 @@ dependencies {
     implementation("com.google.firebase:firebase-admin:9.2.0")
     implementation("com.kborowy:firebase-auth-provider:1.5.0")
 }
-
-tasks {
-    // Existing shadowJar task to create the fat JAR
-    shadowJar {
-        archiveBaseName.set("fotofolio")
-        archiveClassifier.set("")
-        archiveVersion.set("")
-
-        manifest {
-            attributes["Main-Class"] = "cz.cvut.fit.ApplicationKt" // or your custom main class
-        }
-
-        mergeServiceFiles() // Ensure all service files are merged if needed
-    }
-
-    // Add this run configuration to specify the heap size for the fat JAR
-    register<JavaExec>("runFatJarHeapMax") {
-        group = "application"
-        description = "Run the fat JAR with custom heap size"
-        mainClass.set("cz.cvut.fit.ApplicationKt")
-        classpath = sourceSets["main"].runtimeClasspath
-
-        args = listOf("-jar", "build/libs/fotofolio-all.jar")
-
-        // Set the JVM options for heap size
-        jvmArgs = listOf(
-            "-Xmx384m", // Max heap size
-            "-Xms256m"  // Initial heap size
-        )
-    }
-}
